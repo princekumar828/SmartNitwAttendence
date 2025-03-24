a8fb3108270d96add507046d0a3a727ef5ed3b78
princekumar828 <kumarprince282002@gmail.com>
2025-03-24 10:04:14 +0530

    url change

--- app/src/main/java/com/princesaha/attendance/ApiConfig.java
+++ app/src/main/java/com/princesaha/attendance/ApiConfig.java
@@ -2,7 +2,7 @@ package com.princesaha.attendance;
 
 public class ApiConfig {
     // Base URL (Change this to your actual server URL)
-    public static final String BASE_URL = "https://feb4-2409-408a-b16-6c5b-44e-5a69-d740-d6ea.ngrok-free.app/";
+    public static final String BASE_URL = "https://a2c4-2409-408a-b16-6c5b-d919-1552-f755-ec87.ngrok-free.app/";
 
     // API Endpoints
     public static final String  REGISTER_STUDENT_API= BASE_URL + "register_student";
